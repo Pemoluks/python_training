0bd007ff60259141fe8f8aa1d926971bedcf265b
Pemoluks <61008543+Pemoluks@users.noreply.github.com>
2020-04-12 14:03:56 +0300

    Create search.py
    запись рекордером

---
+++ search.py
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta content="text/html; charset=UTF-8" http-equiv="content-type" />
+	<title>search</title>
+</head>
+<body>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
+<tr><td rowspan="1" colspan="3">search</td></tr>
+</thead>
+<tbody>
+<tr><td>open</td><td>https://www.speedtest.net/<datalist><option>https://www.speedtest.net/</option></datalist></td><td></td>
+</tr>
+<tr><td>click</td><td>//div[@id='container']/div[2]/div/div/div/div[2]/div[3]/div/a/span[4]<datalist><option>//div[@id='container']/div[2]/div/div/div/div[2]/div[3]/div/a/span[4]</option><option>//span[4]</option><option>css=span.start-text</option></datalist></td><td></td>
+</tr>
+</tbody></table>
+</body>
+</html>
